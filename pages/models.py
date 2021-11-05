975fa3c4aef7675f15ba9264b6d0af9b98f7dec8
Bret Witt <legonxtboy@gmail.com>
2021-11-05 11:19:29 -1000

    Hike model, dynamic cards based on hike model

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Hike(models.Model):
+    hike_name = models.CharField(max_length=50)
+    hike_difficulty = models.IntegerField(default=3)
+    hike_traffic = models.IntegerField(default=3)
