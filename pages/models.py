1a7ebc898f8e12d09e13b5c161e05b5d7ba07e6c
Bret Witt <legonxtboy@gmail.com>
2021-11-06 13:17:27 -1000

    Remove traffic field

--- pages/models.py
+++ pages/models.py
@@ -4,12 +4,12 @@ from django.db import models
 class Hike(models.Model):
     hike_name = models.CharField(max_length=50)
     hike_difficulty = models.IntegerField(default=3)
-    hike_traffic = models.IntegerField(default=3)
+
     hike_rating = models.IntegerField(default=4)
     hike_images = models.CharField(max_length=50, default="")
     hike_description = models.CharField(max_length=500, default="")
     hike_location = models.CharField(max_length=50, default="")
     hike_route = models.CharField(max_length=500, default="")
-    
+
     def __str__(self):
         return self.hike_name
