6ee765e64de15a3d82dd88b9b6c5e9ac2585d58a
Bret Witt <legonxtboy@gmail.com>
2021-11-06 13:15:56 -1000

    Updated hike model to include description & route

--- pages/models.py
+++ pages/models.py
@@ -7,6 +7,9 @@ class Hike(models.Model):
     hike_traffic = models.IntegerField(default=3)
     hike_rating = models.IntegerField(default=4)
     hike_images = models.CharField(max_length=50, default="")
+    hike_description = models.CharField(max_length=500, default="")
+    hike_location = models.CharField(max_length=50, default="")
+    hike_route = models.CharField(max_length=500, default="")
     
     def __str__(self):
         return self.hike_name
