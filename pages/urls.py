0e7c3e896f88daff901506a53acba2c2bcf49338
Maxwell <maxwellpauly32@gmail.com>
2021-11-06 10:05:03 -1000

    distance calculator

--- pages/urls.py
+++ pages/urls.py
@@ -1,11 +1,5 @@
 from django.urls import path
-<<<<<<< HEAD
 from .views import homePage, discoverPage, contactPage, aboutPage, trailMapPage, pointsVerification
-=======
-from django.conf.urls import include
-
-from .views import homePage, discoverPage, contactPage, aboutPage, trailMapPage, loginPage, signupPage
->>>>>>> 898f7f890b2c301f9f12ee84f9812c3012681f52
 
 urlpatterns = [
     path('', homePage, name="home"),
@@ -13,11 +7,5 @@ urlpatterns = [
     path('contact/', contactPage, name="contact"),
     path('about/', aboutPage, name="about"),
     path('trailmap/', trailMapPage, name="trailmap"),
-<<<<<<< HEAD
     path('pointsVerification/', pointsVerification, name="pointsVerification"),
-=======
-    path("login/", loginPage, name="login"),
-    path("signup/", signupPage, name="signup"),
-
->>>>>>> 898f7f890b2c301f9f12ee84f9812c3012681f52
 ]
