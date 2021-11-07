2d9ca4d751c6c7ae2c690074f692558afaf53bd4
Maxwell <maxwellpauly32@gmail.com>
2021-11-06 19:16:11 -1000

    phone number validation

--- reservations/forms.py
+++ reservations/forms.py
@@ -5,8 +5,8 @@ from django.forms.widgets import DateInput, TimeInput
 class CreateReservationForm(ModelForm):
     class Meta:
         model = Reservation
-        fields = ['hike', 'time', 'date', 'number_of_people']
+        fields = ['hike', 'time', 'date', 'number_of_people', 'emergency_contact_name', 'emergency_contact_phone_number']
         widgets = {
             'date': DateInput(attrs={'type':'date'}),
-            'time': TimeInput(attrs={'type':'time'})
+            'time': TimeInput(attrs={'type':'time'}),
         }
