d27f382e42aca4b9521c9d317c0b8975a61b4223
ashtencodes <ashakemoto@gmail.com>
2021-11-08 18:45:59 -1000

    Adeleted duplicate files

--- donations/urls 2.py
+++
@@ -1,7 +0,0 @@
-
-from django.urls import path
-from .views import donationsPage
-
-urlpatterns = [
-    path("", donationsPage, name="donations")
-]
--- donations/views 2.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def donationsPage(request):
-    return render(request, 'donationsPage.html', {})
-
-
-
