01f60f926e406059b0c4970be4359d34a4f7e6c1
Bret Witt <legonxtboy@gmail.com>
2021-11-13 14:26:52 -1000

    Refactored trailmap JS back to global

--- static/js/trailmap.js
+++ static/js/trailmap.js
@@ -1,3 +1,42 @@
+function setupLeafletMap() {
+    var origin = [21.481056406745278, -157.95845361227276];
+    var zoom = 11;
+
+    var currentdate = new Date();
+    var datearray = getFormattedDateTime(currentdate)
+
+    var date = datearray[0];
+    var time = datearray[1];
+
+    var num_people_n = timeParse(time);
+
+    var mymap = L.map('map', {
+        renderer: L.canvas({ tolerance: 14 })
+        }).setView(origin, zoom);
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox/outdoors-v11',
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: 'pk.eyJ1IjoiYWFrZW1vdG8iLCJhIjoiY2t2bHF1eGVhOWZtdDJwcGdwZWtoMmQ3bCJ9.l2t-FRs7-6a6G3MPgTfowg'
+        }).addTo(mymap);
+        
+    updateMap(mymap, date, num_people_n);
+}
+
+function showMapFromDateTime(date, time) {
+    var parseTime = time/100
+    var slot = Math.floor(parseTime / 4) + 1;
+    var num_people_n = 'num_people_' + slot;
+    updateMap(map, date, num_people_n)
+}
+
+function updateMapFocus(map, hike_id) {
+    changeMapOrigin(map, hike_id)
+}
+
 function changeMapOrigin(map, hike_id){
 
     var selectedHike = Trails.features.filter(t=>t.properties.HIKEID === parseInt(hike_id));
