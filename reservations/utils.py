83cde90bdbfdb31c3256ef728292bb9677eeeb0f
Maxwell <maxwellpauly32@gmail.com>
2021-11-19 20:15:41 -1000

    url fix in text

--- reservations/utils.py
+++ reservations/utils.py
@@ -7,5 +7,5 @@ def queText(reservation):
     now = arrow.now()
     arr = arr.shift(hours=8)
     time = int((arr-now).total_seconds())
-    message = "Hello, {} {} listed you as an emergency contact for their hike. They signed up on https://www.alakai.com, an online platform built to assist hikers with all their hiking needs. {} signed up to go on {} at {} on {}. This message was scheduled to send 8 hours after their expected start time. Would you check up on them and make sure they arrived back safely. Mahalo!".format(user.first_name, user.last_name, user.first_name, reservation.hike.hike_name, reservation.time, reservation.date)
+    message = "Hello, {} {} listed you as an emergency contact for their hike. They signed up on https://www.alakai.xyz, an online platform built to assist hikers with all their hiking needs. {} signed up to go on {} at {} on {}. This message was scheduled to send 8 hours after their expected start time. Would you check up on them and make sure they arrived back safely. Mahalo!".format(user.first_name, user.last_name, user.first_name, reservation.hike.hike_name, reservation.time, reservation.date)
     send_sms.send_with_options(phone_number=reservation.emergency_contact.phone_number, msg=message, delay=time*1000)
